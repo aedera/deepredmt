98c38a59df7c96a64c871ee0029a805c17bc3b78
Alejandro A. Edera <edera.alejandro@gmail.com>
2021-04-13 13:08:00 -0300

    Importing package error fixed

--- deepredmt/layers.py
+++ deepredmt/layers.py
@@ -1,13 +1,6 @@
-import math
-
 import tensorflow as tf
-import tensorflow.keras as keras
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Layer
 
-from src import utils
-
-class L2Normalization(keras.layers.Layer):
+class L2Normalization(tf.keras.layers.Layer):
     def __init__(self, axis, **kwargs):
         super(L2Normalization, self).__init__(**kwargs)
         self.axis = axis
