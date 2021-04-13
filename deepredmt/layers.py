f4c22570fe060b0c1db3e421ded81067e438e7f6
Alejandro A. Edera <edera.alejandro@gmail.com>
2021-04-13 18:54:38 -0300

    New layer

--- deepredmt/layers.py
+++ deepredmt/layers.py
@@ -14,19 +14,46 @@ class L2Normalization(tf.keras.layers.Layer):
     def call(self, x):
         return tf.nn.l2_normalize(x, axis=self.axis)
 
-def Conv1DTranspose(input_tensor,
-                    filters,
-                    kernel_size,
-                    strides=2,
-                    output_padding=None,
-                    padding='same'):
-    x = tf.keras.layers.Lambda(lambda x: K.expand_dims(x, axis=2))(input_tensor)
-    x = tf.keras.layers.Conv2DTranspose(filters=filters,
-                                        kernel_size=(kernel_size, 1),
-                                        strides=(strides, 1),
-                                        output_padding=output_padding,
-                                        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                                        bias_initializer='zeros',
-                                        padding=padding)(x)
-    x = tf.keras.layers.Lambda(lambda x: K.squeeze(x, axis=2))(x)
-    return x
+class Conv1DTranspose(tf.keras.layers.Layer):
+    def __init__(self,
+                 filters,
+                 kernel_size,
+                 strides=2,
+                 output_padding=None,
+                 padding='same', **kwargs):
+        super(Conv1DTranspose, self).__init__(**kwargs)
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.output_padding = output_padding
+        self.padding = padding
+
+    def build(self, input_shape):
+        kinit = tf.keras.initializers.he_normal(seed=1234)
+
+        self.conv = tf.keras.layers.Conv2DTranspose(
+            filters=self.filters,
+            kernel_size=(self.kernel_size, 1),
+            strides=(self.strides, 1),
+            output_padding=self.output_padding,
+            kernel_initializer=kinit,
+            bias_initializer='zeros',
+            padding=self.padding)
+
+    def get_config(self):
+        config = super(Conv1DTranspose, self).get_config()
+
+        config.update({'filters': self.filters})
+        config.update({'kernel_size': self.kernel_size})
+        config.update({'strides': self.strides})
+        config.update({'output_padding': self.output_padding})
+        config.update({'padding': self.padding})
+
+        return config
+
+    def call(self, x):
+        x = tf.keras.layers.Lambda(lambda x: K.expand_dims(x, axis=2))(x)
+        x = self.conv(x)
+        x = tf.keras.layers.Lambda(lambda x: K.squeeze(x, axis=2))(x)
+
+        return x
--- deepredmt/models.py
+++ deepredmt/models.py
@@ -13,97 +13,138 @@ def CAE(input_shape, num_hunits):
     inputs = tf.keras.Input(shape=input_shape)
     x = inputs
     # first convolutional layer
-    x = tf.keras.layers.Conv1D(filters[0],
-                               3,
-                               strides=1,
-                               kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                               bias_initializer='zeros',
-                               kernel_regularizer=tf.keras.regularizers.l2(1e-4),
-                               padding='same')(x)
+    x = tf.keras.layers.Conv1D(
+        filters[0],
+        3,
+        strides=1,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros',
+        kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+        padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
     # second convolutional layer
-    x = tf.keras.layers.Conv1D(filters[0],
-                               3,
-                               strides=1,
-                               kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                               bias_initializer='zeros',
-                               kernel_regularizer=tf.keras.regularizers.l2(1e-4),
-                               padding='same')(x)
+    x = tf.keras.layers.Conv1D(
+        filters[0],
+        3,
+        strides=1,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros',
+        kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+        padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
     # third convolutional layer
-    x = tf.keras.layers.Conv1D(filters[1],
-                               3,
-                               strides=2,
-                               kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                               bias_initializer='zeros',
-                               kernel_regularizer=tf.keras.regularizers.l2(1e-4),
-                               padding='same')(x)
+    x = tf.keras.layers.Conv1D(
+        filters[1],
+        3,
+        strides=2,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros',
+        kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+        padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
     # forth convolutional layer
-    x = tf.keras.layers.Conv1D(filters[2],
-                               3,
-                               strides=2,
-                               kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                               bias_initializer='zeros',
-                               kernel_regularizer=tf.keras.regularizers.l2(1e-4),
-                               padding='same')(x)
+    x = tf.keras.layers.Conv1D(
+        filters[2],
+        3,
+        strides=2,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros',
+        kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+        padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
     # fifth convolutional layer
-    x = tf.keras.layers.Conv1D(filters[3],
-                               3,
-                               strides=2,
-                               kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                               bias_initializer='zeros',
-                               kernel_regularizer=tf.keras.regularizers.l2(1e-4),
-                               padding='valid')(x)
+    x = tf.keras.layers.Conv1D(
+        filters[3],
+        3,
+        strides=2,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros',
+        kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+        padding='valid')(x)
     x = tf.keras.layers.Activation('relu')(x)
     # sixth convolutional layer
-    x = tf.keras.layers.Conv1D(filters[4],
-                               3,
-                               strides=1,
-                               kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                               bias_initializer='zeros',
-                               kernel_regularizer=tf.keras.regularizers.l2(1e-4),
-                               padding='valid')(x)
+    x = tf.keras.layers.Conv1D(
+        filters[4],
+        3,
+        strides=1,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros',
+        kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+        padding='valid')(x)
     x = tf.keras.layers.Activation('relu')(x)
-    x = tf.keras.layers.Conv1D(filters[5],
-                               2,
-                               strides=1,
-                               kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                               bias_initializer='zeros',
-                               kernel_regularizer=tf.keras.regularizers.l2(1e-4),
-                               padding='valid')(x)
+    x = tf.keras.layers.Conv1D(
+        filters[5],
+        2,
+        strides=1,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros',
+        kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+        padding='valid')(x)
     x = tf.keras.layers.Activation('relu')(x)
     # flatten convolutional filters
     x = tf.keras.layers.Flatten()(x)
-    y = tf.keras.layers.Dense(num_hunits,
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                              bias_initializer='zeros')(x)
+    y = tf.keras.layers.Dense(
+        num_hunits,
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros')(x)
     # l2-normalization
     y = L2Normalization(axis=1)(y)
     #
     # decoder
     #
-    x = tf.keras.layers.Dense(2*filters[5],
-                              kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
-                              bias_initializer='zeros')(y)
+    x = tf.keras.layers.Dense(
+        2*filters[5],
+        kernel_initializer=tf.keras.initializers.he_normal(seed=1234),
+        bias_initializer='zeros')(y)
     x = tf.keras.layers.Reshape((2, filters[5]))(x)
     x = tf.keras.layers.Activation('relu')(x)
     # x = layers.ZeroPadding1D((0,1))(x)
-    x = Conv1DTranspose(x, filters=filters[4], kernel_size=2, strides=1, output_padding=None, padding='valid')
+    x = Conv1DTranspose(filters=filters[4],
+                        kernel_size=2,
+                        strides=1,
+                        output_padding=None,
+                        padding='valid')(x)
     x = tf.keras.layers.Activation('relu')(x)
-    x = Conv1DTranspose(x, filters=filters[3], kernel_size=3, strides=1, output_padding=(0, 0), padding='valid')
+
+    x = Conv1DTranspose(filters=filters[3],
+                        kernel_size=3,
+                        strides=1,
+                        output_padding=(0, 0),
+                        padding='valid')(x)
     x = tf.keras.layers.Activation('relu')(x)
-    x = Conv1DTranspose(x, filters=filters[2], kernel_size=3, strides=2, output_padding=(0, 0), padding='valid')
+
+    x = Conv1DTranspose(filters=filters[2],
+                        kernel_size=3,
+                        strides=2,
+                        output_padding=(0, 0),
+                        padding='valid')(x)
     x = tf.keras.layers.Activation('relu')(x)
-    x = Conv1DTranspose(x, filters=filters[1], kernel_size=3, strides=2, output_padding=(0, 0), padding='same')
+
+    x = Conv1DTranspose(filters=filters[1],
+                        kernel_size=3,
+                        strides=2,
+                        output_padding=(0, 0),
+                        padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
-    x = Conv1DTranspose(x, filters=filters[0], kernel_size=3, strides=2, output_padding=(0, 0), padding='same')
+
+    x = Conv1DTranspose(filters=filters[0],
+                        kernel_size=3,
+                        strides=2,
+                        output_padding=(0, 0),
+                        padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
-    x = Conv1DTranspose(x, filters=filters[0], kernel_size=3, strides=1, padding='same')
+
+    x = Conv1DTranspose(filters=filters[0],
+                        kernel_size=3,
+                        strides=1,
+                        padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
+
     # Last transposed convolutional layer used for reconstructing inputs
-    x = Conv1DTranspose(x, filters=input_shape[1], kernel_size=3, strides=1, padding='same')
+    x = Conv1DTranspose(filters=input_shape[1],
+                        kernel_size=3,
+                        strides=1,
+                        padding='same')(x)
     x = tf.keras.layers.Activation('sigmoid')(x)
     # softmax classifier
     z = tf.keras.layers.Dense(2,
