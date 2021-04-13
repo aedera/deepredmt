1f5b68bf275af95f61c4e1b427cfe4078d07d227
Alejandro A. Edera <edera.alejandro@gmail.com>
2021-04-13 13:09:58 -0300

    Importing package error fixed

--- deepredmt/losses.py
+++ deepredmt/losses.py
@@ -1,16 +1,16 @@
-import random
 import tensorflow as tf
-import tensorflow.keras as keras
-from tensorflow.keras import backend as K
 
-from src import utils
-from src import layers
+HYPERPARAM = 0.0001
 
-def classification_loss_fn(class_true, class_pred, ext_true, label_smoothing=True):
+def classification_loss_fn(class_true,
+                           class_pred,
+                           ext_true,
+                           label_smoothing=True):
         # label smoothing
         if label_smoothing:
                 class_true = ext_true * class_true
-        class_loss = tf.keras.losses.categorical_crossentropy(class_true, class_pred)
+        class_loss =
+        tf.keras.losses.categorical_crossentropy(class_true, class_pred)
         return tf.reduce_mean(class_loss)
 
 def reconstruction_loss_fn(true, recon):
@@ -18,5 +18,4 @@ def reconstruction_loss_fn(true, recon):
         return tf.reduce_mean(recon_loss)
 
 def regularization(params):
-        HYPERPARAM = 0.0001
         return tf.add_n([ tf.nn.l2_loss(v) for v in params ]) * HYPERPARAM
